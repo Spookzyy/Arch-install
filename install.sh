d06a41866c10366f33f1cf3d9190c6414ed8a90a
Spookzyy <84041117+Spookzyy@users.noreply.github.com>
2024-07-06 10:33:46 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-yay
-yay -S steam coolercontrol lact easyeffects goverlay
+sudo pacman -S --needed base-devel git 
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+
+yay -S dxvk-bin heroic-games-launcher vpn-unlimited-bin lutris steam waybar goverlay visual-studio-bin
+
