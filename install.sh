cb7dab229842af82e7fe8b67567f2fdfa0ce51cc
Spookzyy <84041117+Spookzyy@users.noreply.github.com>
2024-07-11 09:17:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ sudo pacman -S firefox
 sudo pacman -S waybar otf-font-awesome ttf-arimo-nerd noto-fonts
 
 mkdir -p ~/.config/waybar
+
 cp /etc/xdg/waybar/* ~/.config/waybar/
+
+cp /home/jake/Arch-install/hyprland.dot/waybar ~/.config/waybar
 
 sudo pacman -S --needed base-devel git 
 
@@ -22,8 +25,6 @@ yay -S --needed --noconfirm wine-staging giflib lib32-giflib libpng lib32-libpng
 
 yay -S --needed --noconfirm dialog dosbox dxvk-bin hwloc m4 usbutils make zenity winetricks gst-editing-services
 
-Also if your on linux kernel (standard) I would recommend you install a scheduler, tuned and thermald
-
 yay -S --needed --noconfirm schedtool minq-ananicy-git  thermald tuned
  
 sudo systemctl enable ananicy
@@ -31,6 +32,8 @@ sudo systemctl start ananicy
 sudo tuned-adm profile throughput-performance
 
 sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+
+sudo pacman -R firefox
 
 yay -S steam lact coolercontrol openrgb lutris heroic-games-launcher microsoft-edge-bin 
 
